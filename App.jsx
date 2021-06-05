326729ee281d8f24f766714d72e37eeb1a41b14f
Mohammad-Naved <74970362+Mohammad-Naved@users.noreply.github.com>
2021-06-05 22:36:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer/>
+    </>
+  );
+}
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReactJs</h1>
+      <p>
+        This was an amazing bootcamp taken up by Saurya Sinha. He covered
+        everything from scratch including JavaScript and React.Js.
+      </p>
+    </div>
+  );
+}
